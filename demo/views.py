879d51866e2f47442bfab4514219215e094ad897
Jahed <jahedinamdar786@gmail.com>
2025-08-29 11:01:58 +0530

    api urls views

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import requests
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def get_external_api(request):
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    response = requests.get(url)
+    return Response(response.json())
+
+@api_view(['POST'])
+def create_post(request):
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.post(url, json=request.data)
+    return Response(response.json())
